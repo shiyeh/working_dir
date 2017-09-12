ccea8f3481ce4ca618b0ef594157f3e0f325bba7
Leo Yeh <leoyeh@schmidtelectronics.com>
2017-09-12 02:44:02 +0000

    Delete files.

--- test.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-import sys
-from add_user import *
-
-addUser(userName=sys.argv[1], passWord=sys.argv[2])
--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-log_path=/tmp/fw_update.log
-fw_name=`ls /tmp/*.fw`
-basename=`basename ${fw_name}`
-echo "Start to firmware update. FW version: ${basename}" > ${log_path}
-echo "successful." >> ${log_path}
