150a21b959bd40a4f0f807529a67ac0d730fb67d
shiyeh <shiyehgo@gmail.com>
2017-09-14 03:10:44 +0000

    Bug fixed

--- mon_packet.py
+++ mon_packet.py
@@ -4,9 +4,10 @@ import logging
 
 log = logging.getLogger(__name__)
 maxdata = 50000  # kB
-memfilename = '/tmp/mon_transdata.log'
+memfilename = '/tmp/mon_transdata.txt'
 netcard = '/proc/net/dev'
 nicname = 'ens33'
+cal_intvl = 3  # seconds
 
 
 def checkfile(filename):
@@ -14,7 +15,7 @@ def checkfile(filename):
         pass
     else:
         with open(filename, 'w') as f:
-            f.write('0' + '\n')
+            f.write('0')
 
 
 def get_net_data(nicname):
@@ -45,7 +46,7 @@ def monfirst(filename):
     nowtime = time.strftime('%m-%d %H:%M', time.localtime(time.time()))
     sec = time.localtime().tm_sec
     print nowtime, sec
-    if nowtime == '01-01 00:00':
+    if nowtime == '09-14 03:04':
         if sec < 10:
             with open(filename, 'w') as f:
                 f.write('0')
@@ -64,17 +65,18 @@ def main():
     # print 'totaltrans= {} kbytes'.format(totaltrans)
 
     while True:
-        time.sleep(5)
+        time.sleep(cal_intvl)
         (new_recv, new_send) = get_net_data(nicname)
-        # print('new_recv= {} kbytes, new_send= {} bytes'.format(new_recv, new_send))
+        print('new_recv= {} kbytes, new_send= {} bytes'.format(new_recv, new_send))
         recvdata = (new_recv - recv) / 1024
         senddata = (new_send - send) / 1024
-        # print('recvdata= {} kbytes, senddata= {} kbytes'.format(recvdata, senddata))
-        log.info('Data receive: {} kbytes, send= {} kbytes'.format(recvdata, senddata))
-        totaltrans += float(recvdata)
-        totaltrans += float(senddata)
-        print 'totaltrans= {} kbytes'.format(totaltrans)
-        log.info('Total transmision (Send + Receive)= {} kbytes'.format(totaltrans))
+        print('recvdata= {} kbytes, senddata= {} kbytes'.format(recvdata, senddata))
+        log.info('Data receive: {:.3f} kbytes, send= {:.3f} kbytes'.format(recvdata, senddata))
+        # totaltrans += float(recvdata)
+        # totaltrans += float(senddata)
+        totaltrans = recvdata + senddata
+        print 'totaltrans= {:.3f} kbytes'.format(totaltrans)
+        log.info('Total transmision (Send + Receive)= {:.3f} kbytes'.format(totaltrans))
 
         with open(memfilename, 'w') as memw:
             memw.write(str(totaltrans) + '\n')
