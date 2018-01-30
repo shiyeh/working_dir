81608e2c654b7befe7d400ff5f5f1f5e87b4872b
shiyeh <shiyehgo@gmail.com>
2018-01-30 01:55:45 +0000

    New scpTest.py

--- scpTest.py
+++ scpTest.py
@@ -12,7 +12,7 @@ passwd = 'G420x'
 host = '10.0.10.1'
 
 
-def showMessage():
+def showInfo():
     tkMessageBox.showinfo('Info', 'Success !!!')
 
 
@@ -24,6 +24,29 @@ def clientExit():
     win.destroy()
 
 
+def delLog():
+    _cmd = '/bin/rm -f /opt/log/fwUpdate*.log'
+    host = entry1.get()
+
+    try:
+        # paramiko.util.log_to_file('paramiko.log')
+        s = paramiko.SSHClient()
+        s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        s.connect(hostname=host, username=username, password=passwd)
+        stdin, stdout, stderr = s.exec_command(_cmd)
+        # print 'OUT: ', stdout.read()
+        # # print 'IN: ', stdin.read()
+        # print 'ERROR: ', stderr.read()
+        s.close()
+
+    except Exception as e:
+        showError(e)
+        clientExit()
+    else:
+        print('Success !')
+        showInfo()
+
+
 def uploadFile():
     # SRC_File = 'update-fw-process.py'
     SRC_File = '123.log'
@@ -31,15 +54,14 @@ def uploadFile():
     # For windows, get the stored path
     if hasattr(sys, "_MEIPASS"):
         srcFilePath = os.path.join(sys._MEIPASS, SRC_File)
-    else:
-        pass
+        print srcFilePath
 
     localpath = srcFilePath
     remotepath = '/opt/mlis/' + SRC_File
     host = entry1.get()
 
     try:
-        paramiko.util.log_to_file('paramiko.log')
+        # paramiko.util.log_to_file('paramiko.log')
         paramiko.SSHClient().set_missing_host_key_policy(paramiko.AutoAddPolicy())
         scp = paramiko.Transport((host, port))
         scp.connect(username=username, password=passwd)
@@ -57,7 +79,7 @@ def uploadFile():
         clientExit()
     else:
         print('Success !')
-        showMessage()
+        showInfo()
 
 
 if __name__ == '__main__':
@@ -69,13 +91,16 @@ if __name__ == '__main__':
     label1 = Label(win, text="Where is your machime (IP address) ?")
     label1.grid(row=0, column=0, sticky=W)
     entry1 = Entry(win)
-    entry1.insert(10, host) # Default IP: 10.0.10.1
+    entry1.insert(10, host)   # Default IP: 10.0.10.1
     entry1.grid(row=1, column=0, sticky=W)
 
-    # okButton = Button(win, text="OK", command=delLog)
-    okButton = Button(win, text="Upload", command=uploadFile)
-    okButton.grid(row=1, column=1, sticky=W)
+    uploadButton = Button(win, text="Upload", command=uploadFile)
+    uploadButton.grid(row=1, column=1, sticky=W)
+
+    okButton = Button(win, text="OK to go", command=delLog)
+    okButton.grid(row=2, column=1, sticky=W)
+
     quitButton = Button(win, text="Quit", command=clientExit)
-    quitButton.grid(row=2, column=1, sticky=W)
+    quitButton.grid(row=3, column=1, sticky=W)
 
     win.mainloop()
