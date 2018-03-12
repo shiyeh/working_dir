8221542e6c45f7fb77a469ec97829ddd7f2c3d28
shiyeh <shiyehgo@gmail.com>
2018-03-12 03:22:59 +0000

    ...

--- run_period_ntpd.py
+++ run_period_ntpd.py
@@ -1,21 +1,45 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 import sched, time, os
+import threading
+import logging
+
+log = logging.getLogger(__name__)
+LOG_PATH = '/tmp/run_ntpd.log'
 
 schedule = sched.scheduler(time.time, time.sleep)
 
 
 def run_ntpd(inc):
-    # 安排inc秒後再次運行自己，即周期運行
     schedule.enter(inc, 0, run_ntpd, (inc,))
-    os.system('/usr/bin/python web-update-ntp-sync.py &')
+    _run_ntpd_path = '/opt/mlis/web-update-ntp-sync.py'
+    os.system('/usr/bin/python {} &'.format(_run_ntpd_path))
+    # Popen(['python', _run_ntpd_path], stdout=subprocess.PIPE)
+    # log.debug(p.stdout.read())
 
 
-def main(inc=30):
-    # enter用來安排某事件的發生時間，從現在起第n秒開始啟動
+def main(inc, lock):
+    lock.acquire()
+
+    # Connect to database
+    # con = sqlite3.connect(os.environ["WEB_APP_DBTMP_PATH"])
+    # cur = con.cursor()
+
+    # incTmp = cur.execute(
+    #     "select [[[ping_addr]]] from [[[lan_setting]]];").fetchone()
+    # inc = str(incTmp[0])
     schedule.enter(0, 0, run_ntpd, (inc,))
-    # 持續運行，直到計劃時間隊列變成空為止
     schedule.run()
+
+    # con.close()
+    lock.release()
 
 
 if __name__ == '__main__':
-    main()
+    logging.basicConfig(level=logging.NOTSET, filename=LOG_PATH,
+        format='%(asctime)s %(levelname)s: %(message)s')
+
+    lock = threading.Lock()
+    thread = threading.Thread(target=main, args=(30, lock))
+    # thread.setDaemon(True)
+    thread.start()
--- web-update-ntp-sync.py
+++ web-update-ntp-sync.py
@@ -8,32 +8,24 @@ import subprocess
 
 # schedule = sched.scheduler(time.time, time.sleep)
 log = logging.getLogger(__name__)
-LOG_PATH = '/tmp/ntpd.log'
+LOG_PATH = '/tmp/run_ntpd.log'
 
 
 def ntp_date(server_name='pool.ntp.org'):
-	log.info('Run ntpd !')
 	try:
+		log.info('Run ntpd using: {}'.format(server_name))
 		cmd = 'ntpd -g -q ' + server_name
-		subprocess.check_call(cmd.split())
+		out = subprocess.check_output(cmd.split())
 
 		cmd = 'hwclock -w'
 		subprocess.check_call(cmd.split())
 	except Exception:
-		print('ERROR, PASS')
+		log.debug('Ntpd ERROR, PASS')
 		pass
 	else:
-		log.info('Ntpd Successful!')
-		print('Success. DONE')
+		log.info(out.strip())
+		log.info('Time sync successful: {}'.format(server_name))
 		sys.exit(0)
-	# finally:
-	# 	print('1111111111111')
-	# 	sys.exit(0)
-
-
-# def perform(inc=10, _ntp_server=''):
-# 	schedule.enter(inc, 0, perform, (inc,))
-# 	ntp_date(_ntp_server)
 
 
 def main():
@@ -44,45 +36,21 @@ def main():
 	# ntpTmp = cur.execute(
 	#     "select ping_addr from lan_setting;").fetchone()
 	# _ntp_server = str(ntpTmp[0])
-	_ntp_server_list = 'tick.stdtime.gov.tw;tock.stdtime.gov.tw;clock.stdtime.gov.tw'
-	_ntp_server_list = _ntp_server_list.split(';')
-	print _ntp_server_list
-	# time.sleep(10)
-	# _ntp_server_list = ['tick.stdtime.gov.tw',
-	# 					'tock.stdtime.gov.tw',
-	# 					'clock.stdtime.gov.tw']
+	ntpTmp = 'tick.stdtime.gov.tw;tock.stdtime.gov.tw;clock.stdtime.gov.tw'
+	_ntp_server_list = ntpTmp.split(';')
 
 	# con.close()
 
 	for _ntp_server in _ntp_server_list:
-		# print('ntp_server: {}'.format(_ntp_server))
-		# try:
-		# 	cmd = 'ping -c2 ' + _ntp_server
-		# 	ret = os.system(cmd)
-		# 	print('ret={}'.format(ret))
-		# 	# ret = subprocess.check_call(cmd.split())
-
-		# 	if ret != 0:
-		# 		continue
-
-		# except Exception:
-		# 	print("Error command: %s" % cmd) # 'Some error occured with the command:', cmd
-		# 	sys.exit(1)
 		cmd = 'ping -c2 ' + _ntp_server
 		ret = os.system(cmd)
-		print('ret={}'.format(ret))
-		# ret = subprocess.check_call(cmd.split())
 
 		if ret != 0:
-			print('PING FAILED.')
+			log.debug('PING FAILED: {}, Try to next one.'.format(_ntp_server))
 			continue
 		else:
+			log.info('PING Success: {}'.format(_ntp_server))
 			ntp_date(_ntp_server)
-			# inc = 30
-			# schedule.enter(0, 0, perform, (inc, _ntp_server))
-			# schedule.run()
-
-		# time.sleep(1)
 
 
 if __name__ == '__main__':
